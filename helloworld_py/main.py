e51004dfcc143dbfaad0d30f3b9bb19a14c57b63
TerryGoh <52595052+TerryGoh@users.noreply.github.com>
2020-05-22 19:15:43 +0800

    Add files via upload

--- helloworld_py/main.py
+++ helloworld_py/main.py
@@ -7,6 +7,12 @@ def hello_world(request):
         Response object using
         `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
     """
+    import tensorflow as tf
+    mnist = tf.keras.datasets.mnist
+    
+        (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    x_train, x_test = x_train / 255.0, x_test / 255.0
+ 
     request_json = request.get_json()
     if request.args and 'message' in request.args:
         return request.args.get('message')
