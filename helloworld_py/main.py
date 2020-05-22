c6cbdbb56442029882505d132aa3738b2c563120
TerryGoh <52595052+TerryGoh@users.noreply.github.com>
2020-05-22 19:19:35 +0800

    Add files via upload

--- helloworld_py/main.py
+++ helloworld_py/main.py
@@ -11,7 +11,7 @@ def hello_world(request):
     """
     mnist = tf.keras.datasets.mnist
     
-        (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
     x_train, x_test = x_train / 255.0, x_test / 255.0
  
     request_json = request.get_json()
